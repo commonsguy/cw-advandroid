b01438e7f0fed8f795ddec4be43066905f03d0cc
Mark Murphy <mmurphy@commonsware.com>
2010-03-23 18:25:55 -0400

    Copyright notice updates

--- ListView/Sections/src/com/commonsware/android/listview/SectionedAdapter.java
+++ ListView/Sections/src/com/commonsware/android/listview/SectionedAdapter.java
@@ -1,6 +1,6 @@
 /***
  	Copyright (c) 2008-2010 CommonsWare, LLC
-	portions Copyright (c) 2008 Jeffrey Sharkey
+	portions Copyright (c) 2008-10 Jeffrey Sharkey
 
 	This program is free software: you can redistribute it and/or modify
 	it under the terms of the GNU General Public License as published by
--- SystemEvents/OnBoot/src/com/commonsware/android/sysevents/boot/OnBootReceiver.java
+++ SystemEvents/OnBoot/src/com/commonsware/android/sysevents/boot/OnBootReceiver.java
@@ -1,5 +1,5 @@
 /***
-	Copyright (c) 2009 CommonsWare, LLC
+	Copyright (c) 2009-10 CommonsWare, LLC
 	
 	Licensed under the Apache License, Version 2.0 (the "License"); you may
 	not use this file except in compliance with the License. You may obtain
