bafcef7bb8f88b4d3f84aa6baecdf1ed92971377
Mark Murphy <mmurphy@commonsware.com>
2010-07-04 10:08:42 -0400

    Added instance state logic

--- Views/ColorMixer/src/com/commonsware/android/colormixer/ColorMixer.java
+++ Views/ColorMixer/src/com/commonsware/android/colormixer/ColorMixer.java
@@ -18,12 +18,16 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Color;
+import android.os.Bundle;
+import android.os.Parcelable;
 import android.util.AttributeSet;
 import android.view.View;
 import android.widget.RelativeLayout;
 import android.widget.SeekBar;
 
 public class ColorMixer extends RelativeLayout {
+	private static final String SUPERSTATE="superState";
+	private static final String COLOR="color";
 	private View swatch=null;
 	private SeekBar red=null;
 	private SeekBar blue=null;
@@ -69,6 +73,25 @@ public class ColorMixer extends RelativeLayout {
 												0xFFA4C639));
 			a.recycle();
 		}
+	}
+	
+	@Override
+	public Parcelable onSaveInstanceState() {
+		Bundle state=new Bundle();
+		
+		state.putParcelable(SUPERSTATE, super.onSaveInstanceState());
+		state.putInt(COLOR, getColor());
+
+		return(state);
+	}
+
+	@Override
+	public void onRestoreInstanceState(Parcelable ss) {
+		Bundle state=(Bundle)ss;
+		
+		super.onRestoreInstanceState(state.getParcelable(SUPERSTATE));
+
+		setColor(state.getInt(COLOR));
 	}
 	
 	public OnColorChangedListener getOnColorChangedListener() {
