f89c49cd6239d610ceeb9907fcd47ad7447a691c
Mark Murphy <mmurphy@xps15.(none)>
2012-01-02 09:50:25 -0500

    updated copyright year

--- NFC/URLTagger/src/com/commonsware/android/nfc/url/URLTagger.java
+++ NFC/URLTagger/src/com/commonsware/android/nfc/url/URLTagger.java
@@ -1,6 +1,6 @@
 /***
-  Copyright (c) 2011 CommonsWare, LLC
-  portions Copyright 2011, The Android Open Source Project
+  Copyright (c) 2012 CommonsWare, LLC
+  portions Copyright 2012, The Android Open Source Project
   
   Licensed under the Apache License, Version 2.0 (the "License"); you may not
   use this file except in compliance with the License. You may obtain a copy
