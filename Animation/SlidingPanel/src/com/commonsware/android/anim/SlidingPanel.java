86e7c5b876fd0cf1af6255d0d1c5f1adc9a930c3
Mark Murphy <mmurphy@xps15.(none)>
2012-01-02 09:45:13 -0500

    updated project files and minor cleanup

--- Animation/SlidingPanel/src/com/commonsware/android/anim/SlidingPanel.java
+++ Animation/SlidingPanel/src/com/commonsware/android/anim/SlidingPanel.java
@@ -15,12 +15,10 @@
    
 package com.commonsware.android.anim;
 
-import android.app.Activity;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.util.AttributeSet;
 import android.view.View;
-import android.view.ViewGroup;
 import android.view.animation.AccelerateInterpolator;
 import android.view.animation.Animation;
 import android.view.animation.TranslateAnimation;
